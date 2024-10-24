14480ad14117fe0a159da655321b3ce48764c184
Magali <magali.lamotte@infolegale.fr>
2024-10-24 15:54:37 +0200

    mise en place du crud avec api platform

--- app/src/Entity/Author.php
+++ app/src/Entity/Author.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
+#[ApiResource()]
 class Author
 {
     #[ORM\Id]
